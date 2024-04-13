c95761b4dc082be830784d2b4b6044b013765eb9
Isaac Turner <spacey_sooty@outlook.com>
2024-04-14 02:36:31 +0800

    send css up as well

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use axum::{response::{Html, IntoResponse}, serve, Router, routing::get};
+use axum::{response::Response, routing::get, serve, Router};
 
-async fn indexhtml() -> impl IntoResponse {
-    let markup = tokio::fs::read("index.html").await.unwrap();
+#[axum::debug_handler]
+async fn indexhtml() -> Response<String> {
+    let markup = include_str!("index.html").to_string();
 
-    return Html(markup);
+    return Response::builder()
+        .status(200)
+        .header("Content-Type", "text/html")
+        .body(markup)
+        .unwrap();
+}
+
+#[axum::debug_handler]
+async fn indexcss() -> Response<String> {
+    let markup = include_str!("index.css").to_string();
+
+    return Response::builder()
+        .status(200)
+        .header("Content-Type", "text/css")
+        .body(markup)
+        .unwrap();
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(indexhtml));
+    let app = Router::new()
+        .route("/", get(indexhtml))
+        .route("/index.css", get(indexcss));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
