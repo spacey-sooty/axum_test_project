c3125a932ddfafcf29282a45aa7154e9784fc459
Isaac Turner <spacey_sooty@outlook.com>
2024-04-14 11:51:07 +0800

    sends cpu usage to endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{response::Response, routing::get, serve, Router};
+use axum::{response::{IntoResponse, Response}, routing::get, serve, Json, Router};
 
 #[axum::debug_handler]
 async fn indexhtml() -> Response<String> {
@@ -33,15 +33,32 @@ async fn indexjs() -> Response<String> {
         .unwrap();
 }
 
+#[axum::debug_handler]
+async fn cpuusage() -> impl IntoResponse {
+    let mut sys = sysinfo::System::new();
+    sys.refresh_cpu_usage();
+
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+
+    sys.refresh_cpu_usage();
+
+    let mut data: Vec<f32> = Vec::new();
+
+    for cpu in sys.cpus() {
+        data.push(cpu.cpu_usage());
+    }
+
+    return Json(data);
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(indexhtml))
         .route("/index.css", get(indexcss))
-        .route("/index.js", get(indexjs));
+        .route("/index.js", get(indexjs))
+        .route("/api/get_cpu_usage", get(cpuusage));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
     serve(listener, app).await.unwrap();
 }
